558890fd5977c56c9b2ce5152eb10995ffa82115
CaroGil <dcgil83@gmail.com>
2026-01-26 16:37:46 -0800

    Parte 1 de 3:  ejercicio 10. (Listas de Mapas) Lista de Tareas (JSON)
    a. Crea una List<Map<String, dynamic>> llamada tareasJson.
    b. Añade 3 mapas a la lista. Cada mapa representa una tarea y debe tener: id (int), descripcion (String) y completada (bool).
    c. Inventa los datos para las 3 tareas (asegúrate de que al menos una esté completada y otra no).

--- ejercicio_10.dart
+++ ejercicio_10.dart
@@ -15,3 +15,32 @@ void main() {
     {'id': 3, 'descripcion': 'Comprar pan', 'completada': true},
   ];
 }
+
+/*
+11. (Clases + .fromMap) Clase Tarea
+
+a. Crea una clase Tarea con los atributos: id (int), descripcion (String), completada (bool).
+b. Crea un constructor principal (con parámetros nombrados, como en el ejercicio 9).
+c. Crea un constructor nombrado llamado Tarea.fromMap(Map<String, dynamic>map) que reciba un mapa (como los del ejercicio 10) e inicialice los atributos de la clase.
+*/
+
+class Tarea {
+  int id;
+  String descripcion;
+  bool completada;
+
+  Tarea({
+    // Constructor principal
+    required this.id,
+    required this.descripcion,
+    required this.completada,
+  });
+
+  Tarea.fromMap(Map<String, dynamic> map)
+    : // Constructor nombrado
+      id = map['id'], // Inicializa el atributo id con el valor del mapa
+      descripcion =
+          map['descripcion'], // Inicializa el atributo descripcion con el valor del mapa
+      completada =
+          map['completada']; // Inicializa el atributo completada con el valor del mapa
+}
